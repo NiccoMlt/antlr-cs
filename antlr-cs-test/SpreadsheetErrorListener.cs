2366084a079286283d41697cd64a9d7679a9139e
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2020-04-20 19:14:27 +0200

    feat: add ErrorListener in test project

---
+++ antlr-cs-test/SpreadsheetErrorListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Antlr4.Runtime;
+
+namespace antlr_cs_test
+{
+    public class SpreadsheetErrorListener : BaseErrorListener
+    {                
+        public string Symbol { get; private set; }
+        public StringWriter Writer { get; private set; }
+
+        public SpreadsheetErrorListener(StringWriter writer)
+        {
+            Writer = writer;
+        }
+
+        // if using ANTLR4cs, signature should be:
+        // public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+
+        public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            Writer.WriteLine(msg);
+
+            Symbol = offendingSymbol.Text;
+        }
+    }
+}
